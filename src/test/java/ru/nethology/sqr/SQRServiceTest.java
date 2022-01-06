eda371efeaeed482a9e5d0355ac31f2b9276b602
Moks <gutamax2@mail.ru>
2022-01-06 09:36:06 +0500

    homework fix

--- src/test/java/ru/nethology/sqr/SQRServiceTest.java
+++ src/test/java/ru/nethology/sqr/SQRServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void calculate() {
+    void shouldcalculate() {
         SQRService service = new SQRService();
         int lowerBounds = 200;
         int upperBound = 300;
@@ -17,31 +17,40 @@ class SQRServiceTest {
     }
 
     @Test
-    void calculateExpectedResultIsGreater() {
+    void shouldcalculateExpectedResultIsGreater() {
         SQRService service = new SQRService();
         int lowerBounds = 200;
         int upperBound = 300;
-        int expected = 5;
+        int expected = 3;
         int actual = service.calculate(lowerBounds, upperBound);
         assertEquals(expected, actual);
     }
 
     @Test
-    void calculateLowerBorderMore() {
+    void shouldcalculateLowerBorderMore() {
         SQRService service = new SQRService();
         int lowerBounds = 250;
         int upperBound = 300;
-        int expected = 3;
+        int expected = 2;
         int actual = service.calculate(lowerBounds, upperBound);
         assertEquals(expected, actual);
     }
 
     @Test
-    void calculateSmallBorders() {
+    void shouldcalculateSmallBorders() {
         SQRService service = new SQRService();
         int lowerBounds = 250;
         int upperBound = 251;
-        int expected = 3;
+        int expected = 0;
+        int actual = service.calculate(lowerBounds, upperBound);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldcalculatenumberborder99() {
+        SQRService service = new SQRService();
+        int lowerBounds = 100;
+        int upperBound = 10000;
+        int expected = 90;
         int actual = service.calculate(lowerBounds, upperBound);
         assertEquals(expected, actual);
     }
